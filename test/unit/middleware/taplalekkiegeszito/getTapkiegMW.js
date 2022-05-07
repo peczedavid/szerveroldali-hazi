135612e4da5dd0cf614c77a8525234bb4bbfc20f
peczedavid <peczedavid@gmail.com>
2022-05-07 11:50:46 +0200

    Elso teszt eleje

---
+++ test/unit/middleware/taplalekkiegeszito/getTapkiegMW.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var getTapkiegMW = require('../../../../middleware/taplalekkiegeszito/getTapkiegMW');
+
+describe('getTapkieg middleware ', function () {
+
+  it('should return a tapkieg', function (done) {
+
+  });
+});
