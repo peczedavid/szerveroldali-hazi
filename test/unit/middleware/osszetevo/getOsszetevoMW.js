52ce0606d7c6b551cce00a5557cfa1a4d4285532
peczedavid <peczedavid@gmail.com>
2022-05-07 12:04:13 +0200

    Masik teszt

---
+++ test/unit/middleware/osszetevo/getOsszetevoMW.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var getUserListMW = require('../../../middleware/user/getUserList');
+
+describe('getUserList middleware ', function () {
+
+  it('should return users', function (done) {
+
+  });
+});
