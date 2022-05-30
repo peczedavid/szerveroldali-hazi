88fb1944d05dbe6c1199cd477bf672976520feff
peczedavid <peczedavid@gmail.com>
2022-05-30 12:02:52 +0200

    Javítgatások :pencil:

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ppv33a');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/ppv33a");
 
 module.exports = mongoose;
